f2cb74f9d4766b9de882650c395a27a0ca3cde6c
DmitriBelan <dmitrbelan@gmail.com>
2021-09-24 20:55:56 +0300

    Texts added

--- Tester.py
+++ Tester.py
@@ -1,19 +1,12 @@
-import datetime
+import time
 
-dt = datetime.datetime(2021, 9, 21, 0, 0, 14)
-today = datetime.datetime.now()
+start = time.time()
+print(start)
+#print(time.asctime())
+#print(time.gmtime())
+time.sleep(0.0000000000001)
+stop = time.time()
+print(stop)
+print(stop - start)
 
-today_str = today.strftime('%d.%m.%Y')
-print(today_str)
-print(type(today_str))
 
-dt1 = '30 Mar 21'
-dt2 = 'November 15, 2001, 16:20' 
-new_dt1 = datetime.datetime.strptime(dt1, '%d %b %y')
-new_dt2 = datetime.datetime.strptime(dt2, '%B %d, %Y, %H:%M')
-
-print(new_dt1)
-print(type(new_dt1))
-
-print(new_dt2)
-print(type(new_dt2))
--- Tests/Test_1_21092021/Task_6.py
+++ Tests/Test_1_21092021/Task_6.py
@@ -1,12 +1,13 @@
-users_list = input('Enter your list of items without ",": ').split()
-users_list_sorted = sorted(users_list)
+# users_list = input('Enter your list of items without ",": ').split()
+# users_list_sorted = sorted(users_list)
 
-new_list = []
+# new_list = []
 
-cnt = 0
-for num in users_list_sorted:
-    if num[cnt] == num[cnt + 1]:
-        new_list.append(num[cnt])
-    cnt += 1
+# cnt = 0
+# for num in users_list_sorted:
+  #  if num[cnt] == num[cnt + 1]:
+   #     new_list.append(num[cnt])
+   # cnt += 1
 
-print(new_array)
+# print(new_array)
+
