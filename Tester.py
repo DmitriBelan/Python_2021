1afa20d9246947aeeb50e5c59125931a9f572923
DmitriBelan <dmitrbelan@gmail.com>
2021-09-28 21:00:42 +0300

    Tester with re added

--- Tester.py
+++ Tester.py
@@ -1,7 +1,35 @@
-txt = "?grr?"
+import re
 
-x = txt.strip("?")
+text_to_search = '''
+123123123
+abcdefghijklmnopqurtuvwxyz
+ABCDEFGHIJKLMNOPQRSTUVWXYZ
+1234567890
+Ha HaHa HaHaH
+MetaCharacters (Need to be escaped):
+. ^ $ * + ? { } [ ] \ | ( )
+example.com
+321-555-4321
+123.555.1234
+123*555*1234
+-23*555*5555
+800-555-1234
+900-555-1234
+988-555-2312
+Mr. Jones
+Mr Smith
+Ms Davis
+Mrs. Robinson
+Mr. T
+Mr. O`Rilley
+abc
 
-print(x)
+ball mall hall wall tall
+333-333-3333333333
+'''
 
+sentence = 'Start a sentence Start and then bring it to an+ end bring Start'
 
+pattern = re.compile(r'[a-zA-Z]$')
+matches = pattern.findall(text_to_search)
+print(matches)
