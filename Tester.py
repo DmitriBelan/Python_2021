f2a87a836f4f62985e3f1844f85412a089c158ca
DmitriBelan <dmitrbelan@gmail.com>
2021-10-01 20:56:42 +0300

    Pandas lesson added

--- Tester.py
+++ Tester.py
@@ -1,35 +1,7 @@
-import re
+import pandas as pd
 
-text_to_search = '''
-123123123
-abcdefghijklmnopqurtuvwxyz
-ABCDEFGHIJKLMNOPQRSTUVWXYZ
-1234567890
-Ha HaHa HaHaH
-MetaCharacters (Need to be escaped):
-. ^ $ * + ? { } [ ] \ | ( )
-example.com
-321-555-4321
-123.555.1234
-123*555*1234
--23*555*5555
-800-555-1234
-900-555-1234
-988-555-2312
-Mr. Jones
-Mr Smith
-Ms Davis
-Mrs. Robinson
-Mr. T
-Mr. O`Rilley
-abc
+pd.set_option('display.max_columns', 9)
+pd.set_option('display.max_rows', 156)
 
-ball mall hall wall tall
-333-333-3333333333
-'''
+df = pd.read_csv('csv_files/2019.csv')
 
-sentence = 'Start a sentence Start and then bring it to an+ end bring Start'
-
-pattern = re.compile(r'[a-zA-Z]$')
-matches = pattern.findall(text_to_search)
-print(matches)
