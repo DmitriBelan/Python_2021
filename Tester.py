074ae297fd8958ec3f410fbeb67a086c195e493f
DmitriBelan <dmitrbelan@gmail.com>
2021-10-22 20:54:36 +0300

    GUI lesson added

--- Tester.py
+++ Tester.py
@@ -1,20 +1,17 @@
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.message import EmailMessage
-
-with smtplib.SMTP_SSL('smtp.zone.eu', 465) as server:
+from tkinter import *
+from tkinter import messagebox
 
 
 
-    server.login('python@mrartful.com', 'qwe576!sdf')
+win = Tk()
+win.title('Hello world')
+win.geometry('400x400')
+win.resizable(False, False)
 
-    msg = EmailMessage()
-    msg['From'] = 'python@mrartful.com'
-    msg['To'] = 'dmitrbelan@gmail.com'
-    msg['Subject'] = 'This is test email sent by python script'
-    msg.set_content('Sample email sent by python')
-    msg.add_alternative( f'<h1>Hello I am just a test email created by Python! TEST TEST TEST</h1>', subtype='html')
 
-    server.send_message(msg)
 
+myLabel1 = Label(win, text='Hello').place(x=10, y=10)
+myLabel2 = Label(win, text='world').place(x=10, y=30)
+
+
+win.mainloop()
